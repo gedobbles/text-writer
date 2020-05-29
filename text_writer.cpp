c14302d5e20c2cecd568356a3f0643368dc1f574
gedobbles <gedobbles@web.de>
2020-05-29 15:04:36 +0200

    only send G/M/T-codes to printer

--- text_writer.cpp
+++ text_writer.cpp
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <string>
 #include <iostream>
+#include <regex>
+
 // Linux headers
 #include <fcntl.h> // Contains file controls like O_RDWR
 #include <errno.h> // Error integer and strerror() function
@@ -13,21 +15,33 @@
 
 int main(int argc, char const *argv[]) {
   printf("3D Printer plot writer by gedobbles.\nx to exit\ndevice?[/dev/ttyUSB0] ");
+
   string input, device;
+  //which device to use, Standard: /dev/ttyUSB0
   getline (std::cin, input);
   if(input == ""){
     device = "/dev/ttyUSB0";
   }else{
     device = input;
   }
-
+  //create Serial
   Serial* s = new Serial(device,250000);
+  //say something to get attention
+  s->wln("M117 Ready to plot!");
+
+  std::regex gcode("[GMT].*");
+  std::regex text(":.*");
 
   getline (std::cin, input);
   while (input != "x") {
-    s->wln(input);
+    if(std::regex_match(input, gcode)){
+      s->wln(input);
+    }
+    if(std::regex_match(input, text)){
+      ;//printer.w(input);
+    }
     getline (std::cin, input);
   }
-
+  s->wln("M117 Bye!");
   return 0;
 }
